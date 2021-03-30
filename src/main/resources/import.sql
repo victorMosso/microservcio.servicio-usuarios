ddbdcf1f4b9dd6e90f7b246bb98035f1d948aef4
victor nahum mosso cruz <janifito@hotmail.com>
2021-03-30 03:52:23 -0600

    Se agrega la dependencia para Postgre, se agrega archivo bootstrap.properties para recuperar configuracion de DataSource por medio de Spring.cloud.config

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into usuarios(username,password,enable,nombre,apellido,email) values('vicmoss','$2a$10$Z9dxpnoNLYMyUeGPVDR53uxqa.vEjNcWPv..DHTOXBBQAcD963TlS',1,'victor','mosso','victor.mosso@gmail.com')
-insert into usuarios(username,password,enable,nombre,apellido,email) values('andres','$2a$10$Xa1TcoBqto/S/7bC9NJ2FeOlkEZ9EzT5NLsGDkow8EjUGyABNagKe',1,'andy','guzman','andy.guzman@gmail.com')
+insert into usuarios(username,password,enable,nombre,apellido,email) values('vicmoss','$2a$10$Z9dxpnoNLYMyUeGPVDR53uxqa.vEjNcWPv..DHTOXBBQAcD963TlS',true,'victor','mosso','victor.mosso@gmail.com')
+insert into usuarios(username,password,enable,nombre,apellido,email) values('andres','$2a$10$Xa1TcoBqto/S/7bC9NJ2FeOlkEZ9EzT5NLsGDkow8EjUGyABNagKe',true,'andy','guzman','andy.guzman@gmail.com')
 
 insert into roles(id, nombre) values (1,'ROLE_ADMIN')
 insert into roles(id, nombre) values (2,'ROLE_USER')
